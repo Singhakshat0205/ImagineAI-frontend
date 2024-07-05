fcb863bd0715b7fa94038219c8d73d633292fef4
Singhakshat0205 <90410393+Singhakshat0205@users.noreply.github.com>
2024-07-05 14:17:10 +0530

    main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
        setLoading(true);
 
        try{
-          const res= await axios.get("https://imagine-2j4gfmu5w-akshat-singhs-projects-5fe4f296.vercel.app/api/v1/post");
+          const res= await axios.get("https://imagine-753j1y7i0-akshat-singhs-projects-5fe4f296.vercel.app/api/v1/post");
            
           
             setAllPosts(res.data.data);
